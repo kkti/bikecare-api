735202613f580e9c8958d3e91271110494f18be4
Tomas Dvorak <tomasdvorak@Tomas-MacBook-Air.local>
2025-08-19 16:33:39 +0300

    chore: import Spring Boot project from local files

---
+++ src/main/kotlin/com/bikecare/bikecare_api/BikecareApiApplication.kt
@@ -0,0 +1,11 @@
+package com.bikecare.bikecare_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BikecareApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BikecareApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/bikecare/bikecare_api/BikecareApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bikecare.bikecare_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BikecareApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
